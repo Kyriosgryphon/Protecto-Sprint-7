20db9df8bbb79522910ea47586a3dd1a3a141856
Kyriosgryphon <cecoi09@gmail.com>
2025-07-10 19:02:57 -0600

    primer commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Protecto-Sprint-7
+# Protecto-Sprint-7, es un proyecto para analizar la variedad de vehiculos
+# en venta en la red, analizandon varias caracteristicas de los mismos
+# con diferentes funciones de analisis
---
+++ notebooks/app.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+car_data = pd.read_csv('vehicle_us.csv')
+hist_button = st.button('Construir histograma')
+
+if hist_button:
+    st.write('Creacion de un histograma para el conjunto de datos de anuncios de venta de coches')
+    
+    fig = px.histogram(car_data, x="odometer")
+    
+    st.plotly_chart(fig, use_container_width=True)
+    
+build_histogram = st.checkbox('Construir un histograma')
+
+if build_histogram: 
+    st.write('Construir un histograma para la columna odometro')    
