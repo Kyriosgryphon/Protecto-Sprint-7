9a6a39ed4d0b9532a977b6ed6449ff8ef20d8ead
Kyriosgryphon <cecoi09@gmail.com>
2025-07-13 17:22:14 -0600

    Actualizado dispersion

--- app.py
+++ app.py
@@ -3,16 +3,12 @@ import plotly.express as px
 import streamlit as st
 
 car_data = pd.read_csv('vehicles_us.csv')
-hist_button = st.button('Construir histograma')
-
-if hist_button:
-    st.write('Creacion de un histograma para el conjunto de datos de anuncios de venta de coches')
-    
-    fig = px.histogram(car_data, x="odometer")
-    
+if st.button('Construir histograma'):
+    st.write('Creación de un histograma para la columna odometer')
+    fig = px.histogram(car_data, x='odometer')
     st.plotly_chart(fig, use_container_width=True)
-    
-build_histogram = st.checkbox('Construir un histograma')
 
-if build_histogram: 
-    st.write('Construir un histograma para la columna odometro')    
+if st.button('Construir diagrama de dispersión'):
+    st.write('Creación de un diagrama de dispersión odometer vs price')
+    fig = px.scatter(car_data, x='odometer', y='price')
+    st.plotly_chart(fig, use_container_width=True)
